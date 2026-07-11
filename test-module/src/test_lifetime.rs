67055893b897dcc8dd76be96f9a888e134ecf5e2
Andrew Chi <chifamicom@outlook.com>
2026-07-11 11:35:03 -0400

    fix unintern test

--- test-module/src/test_lifetime.rs
+++ test-module/src/test_lifetime.rs
@@ -78,8 +78,9 @@ fn gc_after_uninterning(env: &Env) -> Result<Value<'_>> {
         env,
         3,
         || {
-            let x = env.intern("xyz")?;
-            env.call("unintern", [x])?;
+            let obarray = env.call("obarray-make", (1,))?;
+            let x = env.call("intern", ("xyz", obarray))?;
+            env.call("unintern", (x, obarray))?;
             Ok(x)
         },
         print,
